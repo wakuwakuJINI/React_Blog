e5c3ce4a5a23ca760708ecdbd6608bd8218200f2
wakuwakuJINI <synthi6785@gmail.com>
2024-09-10 02:17:04 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
-import "./app.css";
-import Header from "./Components/Header";
-import AsideBar from "./Components/AsideBar";
-import Main from "./Components/Board";
-import Footer from "./Components/Footer";
+import { useState } from 'react'
+import './App.css'
+import Header from './assets/components/Header'
+import Footer from './assets/components/Footer'
+import ProfileBox from './assets/components/ProfileBox'
+import Sidebar from './assets/components/Sidebar'
+import MainContent from './assets/components/MainContent'
 
-function App() {
-  return (
-    <>
-      <Header />
-      <div className="container">
-        {/* <AsideBar /> */}
-        <Main />
+
+
+function App() {  
+  const[activeContent,setActiveContent]=useState('todo')
+  return <>
+    <Header/>
+    <div className="layout">
+      <div className="sidebar-container">
+        <ProfileBox/>
+        <Sidebar setActiveContent={setActiveContent}/>
+        
       </div>
-      <Footer />
-    </>
-  );
+      <MainContent activeContent={activeContent}/>
+    </div>
+    <Footer/>
+  </>
 }
 
 export default App;
