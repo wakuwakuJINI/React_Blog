309dc5a572482ea6318c92103f1233d8a07d1d18
wakuwakuJINI <synthi6785@gmail.com>
2024-09-10 02:19:28 +0900

    Update AsideBar.jsx

--- Components/AsideBar.jsx
+++ Components/AsideBar.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-
-export default function AsideBar() {
-  return;
+export default function Sidebar(setActiveContent){
+    return<>
+     <div className="sidebar">
+      <button onClick={() => setActiveContent('todo')}>투두리스트</button>
+      <button onClick={() => setActiveContent('diary')}>하루 일기</button>
+    </div>
+    </>
 }
